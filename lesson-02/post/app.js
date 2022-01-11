17b8e4573ef38412201f484c5403cd60e41548f1
treysonsearle <treyson.searle96@gmail.com>
2022-01-10 18:33:49 -0700

    lesson 2

--- lesson-02/post/app.js
+++ lesson-02/post/app.js
@@ -3,15 +3,15 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/add-product', (req, res, next) => {
   res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
 app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
