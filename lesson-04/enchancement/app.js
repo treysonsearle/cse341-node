cea548b10def9e2053231b56986f54e9363675b9
treysonsearle <treyson.searle96@gmail.com>
2022-01-15 12:07:09 -0700

    mongoose

--- lesson-04/enchancement/app.js
+++ lesson-04/enchancement/app.js
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-   User.findById('5baa2528563f16379fc8a610')
-      .then(user => {
-         req.user = new User(user.name, user.email, user.cart, user._id);
-         next();
-      })
-      .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//    User.findById('61e30f62a95627df2e33fb83')
+//       .then(user => {
+//          req.user = new User(user.name, user.email, user.cart, user._id);
+//          next();
+//       })
+//       .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
