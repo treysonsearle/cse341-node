8b2d318be2cf5617b458d944a29e14141782eccf
treysonsearle <treyson.searle96@gmail.com>
2022-01-29 10:56:43 -0700

    changes

--- lesson-08/backend/routes/professional.js
+++ lesson-08/backend/routes/professional.js
@@ -4,7 +4,7 @@ const professionalController = require('../controllers/professional');
 
 const router = express.Router();
 
-// GET /feed/posts
+
 router.get('/', professionalController.getData);
-// localhost:8080/professional/
+
 module.exports = router;
